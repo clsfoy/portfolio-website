8dc5738f7ab31076a15af557601cf06492081abd
Charles Foy <clsfoy@gmail.com>
2021-01-18 21:39:06 +0000

    update

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,23 +1,113 @@
 import React from "react";
 import PortfolioCard from "./PortfolioCard";
 import Typography from "@material-ui/core/Typography";
+import book from "./Images/book.png";
+import bookshelf from "./Images/bookshelf.png";
+import logo from "./Images/logo.png";
+import messages2 from "./Images/messages2.png";
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // width: "90%",
+    margin: "0 auto",
+    textAlign: "center",
+  },
+  heading: {
+    // fontSize: theme.typography.pxToRem(15),
+
+    fontSize: "12px",
+    textDecoration: "none",
+    letterSpacing: "2px",
+    textTransform: "uppercase",
+    textAlign: "center",
+  },
+}));
+
 const Portfolio = () => {
+  const classes = useStyles();
   return (
     <section id="portfolio">
       <div className="portfolio-container">
-        <Typography
-          style={{
-            color: "#d25b43",
-            textAlign: "right",
-            margin: "2%",
-          }}
-          variant="h4"
-        >
-          Portfolio
-        </Typography>
-        {/* <div className="portfolio"> */}
+        <div className="treat-container">
+          <Typography
+            style={{
+              color: "#d25b43",
+              textAlign: "center",
+              margin: "2%",
+            }}
+            variant="h4"
+          >
+            Treat Your Shelf
+          </Typography>
+          <div className="treat-container-header">
+            <Typography
+              style={{
+                color: "white",
+                textAlign: "center",
+                margin: "2%",
+              }}
+              variant="p"
+            >
+              Treat Your Shelf is a book swapping app designed to encourage
+              reading and facilitate sharing amongst people from all walks of
+              life without ever needing to pay a penny.
+            </Typography>
+            <p className="btn">View GitHub</p>
+            <div className={classes.root}>
+              <Accordion>
+                <AccordionSummary
+                  style={{
+                    background: "#2d3447",
+                    color: "white",
+                    textAlign: "center",
+                  }}
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography className={classes.heading}>More Info</Typography>
+                </AccordionSummary>
+                <AccordionDetails
+                  style={{ background: "#2d3447", color: "white" }}
+                >
+                  <Typography>
+                    Treat Your Shelf is a book swapping app designed to
+                    encourage reading and facilitate sharing amongst local
+                    communities. <br></br>
+                    <br></br>Users can add their unused books to a virtual
+                    bookshelf through an ISBN barcode scanner, view available
+                    books in their area, then arrange a swap through an in-built
+                    messenger. <br></br>A key element of the project was using
+                    {""}
+                    <span> agile practices;</span> Kanban boards for planning,
+                    daily stand-ups, ‘mobbing’ on issues as a team and
+                    <span> pair-programming.</span> We implemented technologies
+                    not covered on the course, such as{" "}
+                    <span>React Hooks & Contexts</span>, React Webcam Barcode
+                    Scanner, Geocode,
+                    <span> Firebase Authentication & Firestore.</span> Firebase
+                    Authentication & Firestore. We designed a restful API with
+                    Express and Postgress.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </div>
+          </div>
+
+          <div class="treat-img">
+            <img src={logo} alt="book"></img>
+            <img src={bookshelf} alt="bookshelf"></img>
+            <img src={book} alt="bookshelf"></img>
+            <img src={messages2} alt="bookshelf"></img>
+          </div>
+        </div>
+        <div className="separator"></div>
         <PortfolioCard></PortfolioCard>
-        {/* </div> */}
       </div>
     </section>
   );
--- src/Components/PortfolioCard.jsx
+++ src/Components/PortfolioCard.jsx
@@ -1,27 +1,118 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-
-const useStyles = makeStyles({
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ncipad from "./Images/nc-ipad.png";
+import ncmob from "./Images/nc-mobile.png";
+const useStyles = makeStyles((theme) => ({
   root: {
-    // maxWidth: 500,
-    color: "#d25b43",
-    background: "#2d3447",
+    // width: "90%",
+    margin: "0 auto",
+    textAlign: "center",
   },
-  media: {
-    height: 140,
+  heading: {
+    // fontSize: theme.typography.pxToRem(15),
+
+    fontSize: "12px",
+    textDecoration: "none",
+    letterSpacing: "2px",
+    textTransform: "uppercase",
+    textAlign: "center",
   },
-});
+}));
+
 const PortfolioCard = () => {
   const classes = useStyles();
   return (
-    <>
-      <div id="nc-news" className="card-container">
-        <div className="portfolio-card">
+    <div className="nc-container">
+      <Typography
+        style={{
+          color: "#d25b43",
+          textAlign: "center",
+          margin: "2%",
+        }}
+        variant="h4"
+      >
+        NC News
+      </Typography>
+      <div>
+        <div className="treat-container-header">
+          <div className={classes.root}>
+            {/* <Accordion>
+              <AccordionSummary
+                style={{
+                  background: "#2d3447",
+                  color: "white",
+                  textAlign: "center",
+                }}
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography className={classes.heading}>More Info</Typography>
+              </AccordionSummary>
+              <AccordionDetails
+                style={{ background: "#2d3447", color: "white" }}
+              >
+                <Typography>
+                  Treat Your Shelf is a book swapping app designed to encourage
+                  reading and facilitate sharing amongst local communities.{" "}
+                  <br></br>
+                  <br></br>Users can add their unused books to a virtual
+                  bookshelf through an ISBN barcode scanner, view available
+                  books in their area, then arrange a swap through an in-built
+                  messenger. <br></br>A key element of the project was using
+                  {""}
+                  <span> agile practices;</span> Kanban boards for planning,
+                  daily stand-ups, ‘mobbing’ on issues as a team and
+                  <span> pair-programming.</span> We implemented technologies
+                  not covered on the course, such as{" "}
+                  <span>React Hooks & Contexts</span>, React Webcam Barcode
+                  Scanner, Geocode,
+                  <span> Firebase Authentication & Firestore.</span> Firebase
+                  Authentication & Firestore. We designed a restful API with
+                  Express and Postgress.
+                </Typography>
+              </AccordionDetails>
+            </Accordion> */}
+          </div>
+        </div>
+        <div class="nc-img">
+          <Typography
+            style={{
+              color: "white",
+              textAlign: "center",
+              margin: "2%",
+            }}
+            variant="p"
+          >
+            A Reddit style discussion board built for both web and mobile with a
+            feature rich user experience. Fully responsive design built with
+            React, Bootstrap and Material UI with full consideration of the
+            React lifecycle and best-practice state management.
+          </Typography>
+          <div className="nc-img-container">
+            <img src={ncipad} alt="nc news ipad"></img>
+            <div className="nc-btn">
+              <a href="https://charlie-nc-news.netlify.app/" className="btn-nc">
+                View GitHub
+              </a>
+              <a href="https://charlie-nc-news.netlify.app/" className="btn-nc">
+                View on the web
+              </a>
+              <a
+                href="https://is-this-reddit.herokuapp.com/api/"
+                className="btn-nc"
+              >
+                API
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* <div className="portfolio-card">
           <Card className={classes.root}>
             <CardActionArea>
               <h2>NC News - Frontend</h2>
@@ -33,7 +124,7 @@ const PortfolioCard = () => {
                     variant="subtitle"
                   >
                     A Reddit style discussion board built for both web and
-                    mobile with a feature rich user experience. ully responsive
+                    mobile with a feature rich user experience. Fully responsive
                     design built with React, Bootstrap and Material UI with full
                     consideration of the React lifecycle and best-practice state
                     management.
@@ -50,8 +141,8 @@ const PortfolioCard = () => {
               </div>
             </CardActions>
           </Card>
-        </div>
-        <div className="portfolio-card">
+        </div> */}
+        {/* <div className="portfolio-card">
           <Card className={classes.root}>
             <CardActionArea>
               <h2>NC News - Backend</h2>
@@ -83,9 +174,9 @@ const PortfolioCard = () => {
               </div>
             </CardActions>
           </Card>
-        </div>
+        </div> */}
       </div>
-    </>
+    </div>
   );
 };
 
